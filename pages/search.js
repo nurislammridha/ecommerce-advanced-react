1e3d666b88c1978b6679fe1ef05ee9a0d3cb9abc
intern14.it <intern14.it@akij.net>
2020-11-04 21:13:24 -0800

    update

--- pages/search.js
+++ pages/search.js
@@ -8,16 +8,18 @@ export default function giftcard(props) {
   return (
     <>
       <MainLayout>
-        <div class="container">
+      {/* <div className="card p-2 shadow-sm ">
+        <div class="container-fluid">
           <div className="row">
             <div className="col-md-3">
               <Sidebar />
             </div>
             <div className="col-md-9">
-              {/* <PageHeding name="notification" /> */}
+              
               <div className="card card-middle">
-                <div className="sidebar-card-title">
-                  <h2>All Notifications</h2>
+                <div className="sidebar-card-title searchtext pl-3 pt-3">
+                  <h2>Showing search results for "products"</h2>
+                  <p>You can also search from category</p>
                 </div>
               </div>
               <div className="card p-2 shadow-sm mt-3">
@@ -74,9 +76,56 @@ export default function giftcard(props) {
                   </Media.Body>
                 </Media>
               </div>
+                <div className="card pt-2 shadow-sm mt-3">
+                <div className="sidebar-card-title searchtext pl-3 pt-3 border-bottom ">
+                  <h2>Showing search results for "products"</h2>
+                  <p>You can also search from category</p>
+                </div>
+                <div className="border-bottom"></div>
+
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-xl-2 col-lg-2">
+              <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="/images/default/chair.png" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+   
+  </Card.Body>
+</Card>
+              </div>
             </div>
           </div>
         </div>
+        </div> */}
+        <div className="card p-2 shadow-sm ">
+        <div class="container-fluid">
+          <div className="row">
+            <div className="col-md-3">
+              <Sidebar />
+            </div>
+            <div className="col-md-9">
+            <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="/images/default/chair.png" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+  
+  </Card.Body>
+</Card>
+            </div>
+            </div>
+            </div>
+            </div>
+
       </MainLayout>
     </>
   );
