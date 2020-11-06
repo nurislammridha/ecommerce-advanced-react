8a5e7b051737b31729154a5553f1480c257a9731
Md. Nazmus Shakib <shakib.corp@akij.net>
2020-11-07 00:52:57 +0600

    Completed Product feature dynamic

--- store/RootReducer.js
+++ store/RootReducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from "redux";
 import VendorReducer from "./redux/vendors/reducers/VendorReducer";
-// import ProductReducer from "../reducers/products/ProductReducer";
+import ProductReducer from "./redux/products/reducers/ProductReducer";
 // import SliderReducer from "./sliders/SliderReducer";
 // import CartReducer from "./orders/CartReducer";
 // import AuthReducer from "./auth/AuthReducer";
 
 export default combineReducers({
-  // product: ProductReducer,
+  product: ProductReducer,
   // slider: SliderReducer,
   // cart: CartReducer,
   // auth: AuthReducer,
---
+++ store/redux/products/reducers/ProductReducer.js
@@ -0,0 +1,32 @@
+import * as Types from "../../../Types";
+
+const initialState = {
+  products: [],
+  loading: false,
+  error: null,
+};
+
+const ProductReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case Types.GET_PRODUCTS_LOADING:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+
+    case Types.GET_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload.data,
+        loading: false,
+      };
+
+    default:
+      return {
+        ...state,
+      };
+      break;
+  }
+};
+
+export default ProductReducer;
