4a71b7f7b970e59a0acce759391d9bdddf753cfd
intern14.it <intern14.it@akij.net>
2020-11-07 22:46:18 -0800

    single product

--- pages/singleproduct.js
+++ pages/singleproduct.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import MainLayout from '../components/layouts/Layout';
 import Rater from "react-rater";
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
+import ShareIcon from '@material-ui/icons/Share';
 
 export default function singleproduct(props) {
   return (
@@ -39,17 +41,43 @@ export default function singleproduct(props) {
                 </div>
                 <div className="col-xl-4 col-lg-4 bg-white">
                     <div className="singleGalleryProduct bg-white rounded">
-                        <h3>Product Tiitle</h3>
+                      <div className="galleryHeading float-left mt-2">
+                        <h3>Product Title</h3>
+                        </div>
+                        <div className="galleryIcon float-right mt-3 ml-2">
+                        <ShareIcon/>Share
+                       
+                       <span className="favIcon border rounded-circle ml-3" >
+                       <FavoriteIcon/>
+                       </span>
+                        </div>
+                        <div className="clearfix"></div>
                        
                        
                  <Rater total={5} rating={2} /><span>39 Ratings & 174 Reviews</span>
                  
-                <h1>৳ 134</h1><span>222</span>
+                
                 <div className="galleryProductDetails">
-                    <p>Brand: Brand Name</p>
-                    <p>Availability: <span>In stock </span> / <sapn>Sold out</sapn></p>
-                    <p>Color:<span className="rounded">Red</span></p><span className="rounded">Red</span>
+                <h1>৳ 134 <span className="text-warning priceOff">৳ 222</span></h1>
+                    <p>Brand: <b>Brand Name</b></p>
+                    <p>Availability: <span className="text-green" >In stock </span> / <sapn className="text-danger">Sold out</sapn></p>
+                   <p>Color:<span class="colorType border rounded text-dark">Red</span>
+                   <span class="colorTypeTwo border rounded text-primary ">Blue</span>
+                   <span class="colorType border rounded text-dark">Yellow</span></p>
+                   <p>Size:<span class="colorType border rounded text-dark">XS</span>
+                   <span class="colorType border rounded text-dark ">S</span>
+                   <span class="colorType  border rounded text-dark">M</span>
+                   <span class="colorType  border rounded text-dark">L</span>
+                   <span class="colorTypeTwo border rounded text-primary ">XL</span>
+                   <span class="colorType border rounded text-dark ">XXL</span>
+                   </p>
+                   <p>Qty:</p>
 
+                </div>
+                <div clasNAme="galleryButton">
+               <button className="btn btn-light galleryAdd">Add to cart</button>
+              
+                <button className="btn btn-light galleryBuy">Buy Now</button>
                 </div>
                     </div>
 
