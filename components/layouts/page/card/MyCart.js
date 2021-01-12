3d123c1f5bafee523be5187c1bbc0a460693b96e
ma-foyez <mafoyez-bd@gmail.com>
2021-01-13 03:24:20 +0600

    delte cart products almost done

--- components/layouts/page/card/MyCart.js
+++ components/layouts/page/card/MyCart.js
@@ -6,6 +6,7 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import { useDispatch, useSelector } from "react-redux";
 import LoadingSkelleton from "../../../master/skelleton/LoadingSkelleton";
 import {
+  deleteCartItemAction,
   getCartsAction,
   updateCartQtyAction,
 } from "../../../../store/actions/orders/CartAction";
@@ -28,120 +29,124 @@ const MyCart = ({ router }, props) => {
     carts.find((item) => item.productID === id && setQuantity((item.quantity += 1)));
     dispatch(updateCartQtyAction(id, (quantity += 1)));
   };
-//decrease quantity
+  //decrease quantity
   const decrementQunatity = (id, quantity) => {
     console.log('quantity :>> ', quantity);
     carts.find((item) => item.productID === id && item.quantity > 1 && setQuantity((item.quantity -= 1)))
     dispatch(updateCartQtyAction(id, (quantity -= 1)));
   };
 
+  //delete cart products
+  const deleteCartProduct = (id) => {
+    dispatch(deleteCartItemAction(id))
+  }
 
-return (
-  <>
-    <div className="wishbanner pb">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-8">
-            <div className="wishlisttitle">
-              <h1>My Cart</h1>
-            </div>
+  return (
+    <>
+      <div className="wishbanner pb">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8">
+              <div className="wishlisttitle">
+                <h1>My Cart</h1>
+              </div>
 
-            {/* {!loading && carts.map.length === 0 && (
+              {/* {!loading && carts.map.length === 0 && (
                 <div>No Cart Found !!</div>
               )} */}
 
-            {loading && (
-              <LoadingSkelleton
-                alignment="vertical"
-                count={1}
-                width={730}
-                height={200}
-              />
-            )}
+              {loading && (
+                <LoadingSkelleton
+                  alignment="vertical"
+                  count={1}
+                  width={730}
+                  height={200}
+                />
+              )}
 
-            {carts.map((item) => (
-              <div className="mt-2 p-3">
-                <div className="innerwishlist productorderdetailsbox cartlistsection">
-                  <div className="singleorderproduct">
-                    <img
-                      className="img-fluid w-75 m-2"
-                      src={item.productImage}
-                    />
-                  </div>
-                  <div className="wishsingleproductText mycarttext">
-                    <h1>{item.productName}</h1>
-                    <h2>Seller: Shop no</h2>
-                    <h4>৳ {item.price}</h4>
-                    <h2>
-                      Quantity :
+              {carts.map((item) => (
+                <div className="mt-2 p-3">
+                  <div className="innerwishlist productorderdetailsbox cartlistsection">
+                    <div className="singleorderproduct">
+                      <img
+                        className="img-fluid w-75 m-2"
+                        src={item.productImage}
+                      />
+                    </div>
+                    <div className="wishsingleproductText mycarttext">
+                      <h1>{item.productName}</h1>
+                      <h2>Seller: Shop no</h2>
+                      <h4>৳ {item.price}</h4>
+                      <h2>
+                        Quantity :
                         <button
-                        className="btn btn-light quantity-btn decrement bg-light border rounded-circle text-dark ml-3"
-                        onClick={(id, quantity) =>
-                          decrementQunatity(item.productID, item.quantity)
-                        }
-                      >
-                        {" "}
-                        <Remove />
-                      </button>
-                      <span className="colorType border rounded text-dark">
-                        {item.quantity}
-                      </span>
-                      <button
-                        className="btn btn-light quantity-btn  increment bg-light border rounded-circle text-dark ml-2"
-                        onClick={(id, quantity) =>
-                          increaseQuantity(item.productID, item.quantity)
-                        }
-                      >
-                        <AddIcon />
-                      </button>
-                    </h2>
-                  </div>
-                  {/* <div className="mycartquantity">
+                          className="btn btn-light quantity-btn decrement bg-light border rounded-circle text-dark ml-3"
+                          onClick={(id, quantity) =>
+                            decrementQunatity(item.productID, item.quantity)
+                          }
+                        >
+                          {" "}
+                          <Remove />
+                        </button>
+                        <span className="colorType border rounded text-dark">
+                          {item.quantity}
+                        </span>
+                        <button
+                          className="btn btn-light quantity-btn  increment bg-light border rounded-circle text-dark ml-2"
+                          onClick={(id, quantity) =>
+                            increaseQuantity(item.productID, item.quantity)
+                          }
+                        >
+                          <AddIcon />
+                        </button>
+                      </h2>
+                    </div>
+                    {/* <div className="mycartquantity">
                         <NumericInput mobile className="form-control" />
                       </div> */}
 
-                  <div className="orderquantity mycarttext">
-                    <h2>REMOVE</h2>
-                  </div>
-                  <div className="mycartquantity one">
-                    <FavoriteIcon />
+                    <div style={{cursor: 'pointer'}} className="orderquantity mycarttext cursor-pointer" onClick={(id) => deleteCartProduct(item.productID)}>
+                      <h2>REMOVE</h2>
+                    </div>
+                    <div className="mycartquantity one">
+                      <FavoriteIcon />
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
-
-          <div className="col-lg-4">
-            <div className="wishlisttitle">
-              <h1>Price Details</h1>
+              ))}
             </div>
 
-            <div className="promocodesection mt-4">
-              <div className="promotionsummery one">
-                <div className="orderProductDetails cartline">
-                  <div className="orderProduct">
-                    <p>Price (0 items)</p>
-                    <p>Number of Items</p>
-                    <p>Total Price</p>
-                  </div>
-                  <div className="orderProductText">
-                    <p>৳ 500</p>
-                    <p> 2</p>
-                    <p>৳ 500</p>
-                  </div>
-                </div>
+            <div className="col-lg-4">
+              <div className="wishlisttitle">
+                <h1>Price Details</h1>
+              </div>
 
-                <div className="mycartplace">
-                  <button>PLACE ORDER</button>
+              <div className="promocodesection mt-4">
+                <div className="promotionsummery one">
+                  <div className="orderProductDetails cartline">
+                    <div className="orderProduct">
+                      <p>Price (0 items)</p>
+                      <p>Number of Items</p>
+                      <p>Total Price</p>
+                    </div>
+                    <div className="orderProductText">
+                      <p>৳ 500</p>
+                      <p> 2</p>
+                      <p>৳ 500</p>
+                    </div>
+                  </div>
+
+                  <div className="mycartplace">
+                    <button>PLACE ORDER</button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
 };
 
 export default MyCart;
--- store/actions/orders/CartAction.js
+++ store/actions/orders/CartAction.js
@@ -28,6 +28,7 @@ export const getCartsAction = () => async (dispatch) => {
   const data = getCartData();
   dispatch({ type: Types.GET_CARTS, payload: data });
 };
+
 //update cart products quantity
 export const updateCartQtyAction = (product_id, quantity) => async (dispatch) => {
   const cartStorageData = localStorage.getItem("carts");
@@ -50,10 +51,10 @@ export const updateCartQtyAction = (product_id, quantity) => async (dispatch) =>
       localStorage.setItem("carts", JSON.stringify(data.carts));
     }
   }
-
-  // dispatch({ type: Types.UPDATE_CARTS_DATA, payload: getCartData() });
+  dispatch({ type: Types.UPDATE_CARTS_DATA, payload: getCartData() });
 };
 
+//delete cart product
 export const deleteCartItemAction = (product_id) => async (dispatch) => {
   const cartStorageData = localStorage.getItem("carts");
   let data = {
@@ -65,21 +66,10 @@ export const deleteCartItemAction = (product_id) => async (dispatch) => {
     data.carts = JSON.parse(cartStorageData);
     data.products = data.carts.products;
 
-    if (data.carts.products.length === 1) {
-      data.carts.products = [];
-    } else {
-      let findProducts = data.carts.products.filter(function (x) {
-        return x.id !== product_id;
-      });
+    let findProducts = data.carts.filter((item) => item.productID !== product_id); 
 
-      if (findProducts.length > 0) {
-        data.carts.products = findProducts;
-      }
-    }
-
-    localStorage.setItem("carts", JSON.stringify(data.carts));
+    localStorage.setItem("carts", JSON.stringify(findProducts));
   }
-
   dispatch({ type: Types.DELETE_CARTS_DATA, payload: getCartData() });
 };
 
@@ -91,51 +81,53 @@ export const postEmptyCartDeleteMessage = () => async (dispatch) => {
   dispatch({ type: Types.EMPTY_CART_DELETE_MESSAGE, payload: true });
 };
 
-function addToCartData(product, quantity) {
-  // Find product first where product_id = this product_id and
-  // then if exist increment that totalQuantity or add totalCount
-  let products = [];
-  const cartStorageData = localStorage.getItem("carts");
-  if (typeof cartStorageData !== "undefined" && cartStorageData !== null) {
-    let carts = JSON.parse(cartStorageData);
-    if (carts.products.length > 0) {
-      const findProducts = carts.products.filter(function (x) {
-        return x.id === product.id;
-      });
-      if (findProducts.length > 0) {
-        const getProductIndex = carts.products.indexOf(findProducts[0]);
-        findProducts[0].qty = findProducts[0].qty + quantity;
-        carts.products[getProductIndex] = findProducts[0];
-        carts = {
-          products: carts.products,
-        };
-      } else {
-        product.qty = 1;
-        carts.products.push(product);
-        carts = {
-          products: carts.products,
-        };
-      }
-      localStorage.setItem("carts", JSON.stringify(carts));
-    } else {
-      // Add as a first item in carts
-      product.qty = 1;
-      products.push(product);
-      let carts = {
-        products: products,
-      };
-      localStorage.setItem("carts", JSON.stringify(carts));
-    }
-  } else {
-    // Add as a first item in carts
-    product.qty = 1;
-    products.push(product);
-    let carts = {
-      products: products,
-    };
-    localStorage.setItem("carts", JSON.stringify(carts));
-  }
-}
+// function addToCartData(product, quantity) {
+//   // Find product first where product_id = this product_id and
+//   // then if exist increment that totalQuantity or add totalCount
+
+
+//   let products = [];
+//   const cartStorageData = localStorage.getItem("carts");
+//   if (typeof cartStorageData !== "undefined" && cartStorageData !== null) {
+//     let carts = JSON.parse(cartStorageData);
+//     if (carts.products.length > 0) {
+//       const findProducts = carts.products.filter(function (x) {
+//         return x.id === product.id;
+//       });
+//       if (findProducts.length > 0) {
+//         const getProductIndex = carts.products.indexOf(findProducts[0]);
+//         findProducts[0].qty = findProducts[0].qty + quantity;
+//         carts.products[getProductIndex] = findProducts[0];
+//         carts = {
+//           products: carts.products,
+//         };
+//       } else {
+//         product.qty = 1;
+//         carts.products.push(product);
+//         carts = {
+//           products: carts.products,
+//         };
+//       }
+//       localStorage.setItem("carts", JSON.stringify(carts));
+//     } else {
+//       // Add as a first item in carts
+//       product.qty = 1;
+//       products.push(product);
+//       let carts = {
+//         products: products,
+//       };
+//       localStorage.setItem("carts", JSON.stringify(carts));
+//     }
+//   } else {
+//     // Add as a first item in carts
+//     product.qty = 1;
+//     products.push(product);
+//     let carts = {
+//       products: products,
+//     };
+//     localStorage.setItem("carts", JSON.stringify(carts));
+//   }
+// }
 
 function getCartData() {
   const cartStorageData = localStorage.getItem("carts");
