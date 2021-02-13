912a7df0fc7ab191f5c421fc498b714cc80821eb
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-02-14 01:42:46 +0600

    Updated Sidebar

--- components/layouts/page/product/MultipleProducts.js
+++ components/layouts/page/product/MultipleProducts.js
@@ -4,7 +4,7 @@ import Rater from "react-rater";
 import Link from "next/link";
 
 
-import {Form} from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import ProductList from "./ProductList";
 const MultipleProducts = ({ router }, props) => {
   return (
@@ -14,72 +14,92 @@ const MultipleProducts = ({ router }, props) => {
           <div className="row">
             <div className="col-lg-2">
               <div className="filterSideBar ml-3">
-              <div className="Category">
+                <div className="sidebar-section">
                   <h6>Category</h6>
                   <Form>
-                  <Form.Group controlId="formBasicCheckbox">   
-                  <Form.Check type="checkbox" label="Men" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicCheckbox">   
-                  <Form.Check type="checkbox" label="Women" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicCheckbox">   
-                  <Form.Check type="checkbox" label="Men" />
-                  </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Men" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Women" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Men" />
+                    </Form.Group>
                   </Form>
                   <Link href="/">View More</Link>
                 </div>
-                <div className="Brand mt-3">
+
+                <div className="sidebar-section">
                   <h6>Brand</h6>
                   <Form>
-                  <Form.Group controlId="formBasicCheckbox">   
-                  <Form.Check type="checkbox" label="Richman" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicCheckbox">   
-                  <Form.Check type="checkbox" label="Lubnan" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicCheckbox">   
-                  <Form.Check type="checkbox" label="Cats eye" />
-                  </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Richman" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Lubnan" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Cats eye" />
+                    </Form.Group>
                   </Form>
                   <Link href="/">View More</Link>
-                  
+
                 </div>
-                <div className="Type  mt-3">
+
+                <div className="sidebar-section">
+                  <h6>Price</h6>
+                  <div className="row">
+                    <div className="col-5 m-0">
+                      <input type="text" className="form-control" placeholder="Min" />
+                    </div>
+                    <div className="col-5 m-0">
+                      <input type="text" className="form-control" placeholder="Max" />
+                    </div>
+                    <div className="col-2 m-0">
+                      <i className="btn btn-warning fa fa-search"></i>
+                    </div>
+                  </div>
+                  <Link href="/">View More</Link>
+
+                </div>
+
+                <div className="sidebar-section">
                   <h6>Type</h6>
                   <Form>
-                  <Form.Group controlId="formBasicCheckbox">   
-                  <Form.Check type="checkbox" label="Richman" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicCheckbox">   
-                  <Form.Check type="checkbox" label="Lubnan" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicCheckbox">   
-                  <Form.Check type="checkbox" label="Cats eye" />
-                  </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Richman" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Lubnan" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                      <Form.Check type="checkbox" label="Cats eye" />
+                    </Form.Group>
                   </Form>
                   <Link href="/">View More</Link>
-                  
+
                 </div>
-                <div className="Rating  mt-3 mb-2">
+
+                <div className="sidebar-section">
                   <h6>Rating</h6>
-                
-                  
+
+
                   <Rater total={5} rating={2} /><br></br>
-                  
+
                   <Rater total={5} rating={2} /><br></br>
                   <Rater total={5} rating={2} /><br></br>
                   <div className="ratingiew">
-                  <Link href="/">View More</Link>
+                    <Link href="/">View More</Link>
                   </div>
 
-                
-                  
+
+
                 </div>
               </div>
             </div>
             <div className="col-lg-10">
-                <ProductList />
+              <ProductList />
             </div>
           </div>
         </div>
